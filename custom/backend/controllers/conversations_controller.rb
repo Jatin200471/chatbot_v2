ec8f3d512bf62926a5b3f5eed060938ddc9ec571
jAtInn71 <jatinkavani69775@gmail.com>
2026-05-15 11:22:21 +0530

    fix: remove invalid skip_before_action for non-existent callback in conversations controller

--- custom/backend/controllers/conversations_controller.rb
+++ custom/backend/controllers/conversations_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Widget::ConversationsController < Api::V1::Widget::BaseController
   include Events::Types
   before_action :render_not_found_if_empty, only: [:toggle_typing, :toggle_status, :set_custom_attributes, :destroy_custom_attributes]
-  skip_before_action :set_conversation, only: [:inbox_config]
 
   def index
     @conversation = conversation
