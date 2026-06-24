12e247aa5b9c45470157a3674391d3131d34b21d
jAtInn71 <jatinkavani69775@gmail.com>
2026-06-24 17:40:15 +0530

    perfect image

--- custom/widget/components/ElevenLabsVoiceButton.vue
+++ custom/widget/components/ElevenLabsVoiceButton.vue
@@ -111,8 +111,8 @@ export default {
         return;
       }
 
-      const w = 380;
-      const h = 520;
+      const w = 320;
+      const h = 450;
       const left = window.screenX + Math.round((window.outerWidth - w) / 2);
       const top = window.screenY + Math.round((window.outerHeight - h) / 2);
 
